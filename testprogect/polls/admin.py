05573a1d7d29b751b9bec41e57dd51fc4b47bfcc
MarioGezze <mariogezze71147@gmail.com>
2024-02-20 14:43:47 +0700

    removed the unnecessary

--- testprogect/polls/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem
-
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'parent', 'url', 'named_url', 'menu_name')
-
-admin.site.register(MenuItem, MenuItemAdmin)
